df84ac787954154a5ab865edb9af91404ec9d420
abarne <celius377@gmail.com>
2020-01-09 13:02:35 -0600

    ..

--- bookmark-manager/src/App.js
+++ bookmark-manager/src/App.js
@@ -4,6 +4,7 @@ import MainCat from './mainCat/MainCat.js';
 
 const App = () => {
 	let [ mainCat, setMainCat ] = useState([]);
+	let [ newMain, setNewMain ] = useState('');
 
 	useEffect(() => {
 		axios
@@ -16,11 +17,41 @@ const App = () => {
 				console.log('could not get data, ', error);
 			});
 	}, []);
+	useEffect(
+		() => {
+			axios
+				.post(`localhost:4000/api/main/add`, newMain)
+				.then((response) => {
+					console.log(response);
+				})
+				.catch((error) => {
+					console.log('error adding new category, ', error);
+				});
+		},
+		[ newMain ]
+	);
 
 	if (mainCat.length === 0) {
 		return <h1>You have no bookmark categories set yet</h1>;
 	}
-	return <div>{mainCat.map((item) => <MainCat data={item} />)}</div>;
+	return (
+		<div>
+			<div className="titleAndButton">
+				<h1 className="mainPageTitle">Your Bookmark Categories</h1>
+				<label>
+					New Category:{' '}
+					<input
+						className="mainCatInput"
+						type="text"
+						value={newMain}
+						onChange={(e) => setNewMain(e.target.value)}
+					/>
+				</label>
+				<input type="submit" value="Submit" />
+			</div>
+			<div className="mainCatDiv">{mainCat.map((item) => <MainCat data={item} />)}</div>;
+		</div>
+	);
 };
 
 export default App;
