e866272534e1264764ca9545c14c12b667302707
abarne <celius377@gmail.com>
2020-01-09 11:54:27 -0600

    MainCat component made with data flowing into it

--- bookmark-manager/src/App.js
+++ bookmark-manager/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MainCat from './mainCat/MainCat.js';
 
 const App = () => {
-	return (
-		<div>
-			<h1>Testing</h1>
-		</div>
-	);
+	let [ mainCat, setMainCat ] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:4000/api/main`)
+			.then((response) => {
+				console.log('get main cat response.data, ', response.data);
+				setMainCat(response.data);
+			})
+			.catch((error) => {
+				console.log('could not get data, ', error);
+			});
+	}, []);
+
+	if (mainCat.length === 0) {
+		return <h1>You have no bookmark categories set yet</h1>;
+	}
+	return <div>{mainCat.map((item) => <MainCat data={item} />)}</div>;
 };
 
 export default App;
