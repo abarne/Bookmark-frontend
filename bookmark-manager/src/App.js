1450d11cfce1f97d39b7a3dc41e5c5f60f05ea24
abarne <celius377@gmail.com>
2020-01-15 10:31:53 -0600

    testing gpg key added

--- bookmark-manager/src/App.js
+++ bookmark-manager/src/App.js
@@ -5,11 +5,10 @@ import axios from 'axios';
 import MainCat from './components/MainCat.js';
 import Home from './components/Home.js';
 import SubCat from './components/SubCat.js';
-//import Navigation from './comonents/Navigation.js';
 import { Route } from 'react-router-dom';
 
 const store = createStore(model);
-
+//
 function App() {
 	return (
 		<StoreProvider store={store}>
